1ae894bdf95a2336fece747cd97736541812fbb3
Samuel Robinson <pinkstarsr11@gmail.com>
2018-07-25 23:40:13 -0700

    FAILED IDEAS

--- Sudowoodoku/SimpleSudoku.cs
+++ Sudowoodoku/SimpleSudoku.cs
@@ -18,35 +18,61 @@ namespace Sudowoodoku {
 			return null;
 		}
 
+		private List<int> getPossibilities(int x,int y) {
+
+			bool[] removed = { false,false,false,false,false,false,false,false,false,false };
+
+			//Inner quadrant scan
+			for(int yIndex = 0;yIndex<9;yIndex++) {
+				removed[board[x,yIndex]] = true;
+			}
+
+			//Horizontal left to right scan
+			//todo
+
+			var row = x / 3;
+			var subRow = y / 3;
+
+			for(int horizontalIndex = 0;horizontalIndex<9;horizontalIndex++) {
+
+				var xValue = (horizontalIndex / 3) + row;
+				var yValue = (horizontalIndex % 3) + (subRow * 3);
+				removed[board[xValue,yValue]] = true;
+
+
+			}
+
+			//Vertical top to bottom scan
+			//todo
+
+
+			var column = x % 3;
+			var subColumn = y % 3;
+
+			for(int verticalIndex = 0;verticalIndex<9;verticalIndex++) {
+
+				var xValue = ((verticalIndex / 3)*3) + column;
+				var yValue = ((verticalIndex % 3)*3) + subColumn;
+				removed[board[xValue,yValue]] = true;
+
+
+			}
+
+			List<int> possibilities = new List<int>();
+
+			for(int i = 1;i<10;i++) {
+				if(!removed[i]) {
+					possibilities.Add(i);
+				}
+			}
+
+			return possibilities;
+		}
+
 		public void PopulateBoard(int seed,int givenTiles) {
 
 			int[,] board = new int[9,9];
 
-			List<int> getPossibilities(int x,int y) {
-
-				bool[] removed = {false,false,false,false,false,false,false,false,false,false };
-
-				//Inner quadrant scan
-				for(int yIndex = 0;yIndex<9;yIndex++) {
-					removed[board[x,yIndex]] = true;
-				}
-
-				//Horizontal left to right scan
-				//todo
-
-				//Vertical top to bottom scan
-				//todo
-
-				List<int> possibilities = new List<int>();
-
-				for(int i = 1;i<10;i++) {
-					if(!removed[i]) {
-						possibilities.Add(i);
-					}
-				}
-
-				return possibilities;
-			}
 
 			Random random = new Random(seed);
 
@@ -56,7 +82,9 @@ namespace Sudowoodoku {
 
 					var possibilities = getPossibilities(x,y);
 
-				board[x,y] = possibilities[
+
+					//This doesn't work :(
+					board[x,y] = possibilities[
 						random.Next(0,possibilities.Count-1)
 					];
 
